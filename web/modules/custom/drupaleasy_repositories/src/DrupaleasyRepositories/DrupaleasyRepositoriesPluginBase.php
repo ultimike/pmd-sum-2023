8ec377ff29f276dd745bd6f1ebde2a11d60556ee
Adrian Young <young@gilderlehrman.org>
2023-02-28 14:37:50 -0600

    End of class February 28.

--- web/modules/custom/drupaleasy_repositories/src/DrupaleasyRepositories/DrupaleasyRepositoriesPluginBase.php
+++ web/modules/custom/drupaleasy_repositories/src/DrupaleasyRepositories/DrupaleasyRepositoriesPluginBase.php
@@ -30,4 +30,32 @@ abstract class DrupaleasyRepositoriesPluginBase extends PluginBase implements Dr
     return FALSE;
   }
 
+  /**
+   * Build array of a single repository.
+   *
+   * @param string $machine_name
+   *   The full name of the repository.
+   * @param string $label
+   *   The short name of the repository.
+   * @param string|null $description
+   *   The description of the repository.
+   * @param int $num_open_issues
+   *   The number of open issues.
+   * @param string $url
+   *   The URI of the repository.
+   *
+   * @return array
+   *   An array containing info about a single repository.
+   */
+  protected function mapToCommonFormat(string $machine_name, string $label, string|null $description, int $num_open_issues, string $url): array {
+    $repo_info[$machine_name] = [
+      'label' => $label,
+      'description' => $description,
+      'num_open_issues' => $num_open_issues,
+      'source' => $this->getPluginId(),
+      'url' => $url,
+    ];
+    return $repo_info;
+  }
+
 }
---
+++ web/modules/custom/drupaleasy_repositories/src/Plugin/DrupaleasyRepositories/YmlRemote.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\drupaleasy_repositories\Plugin\DrupaleasyRepositories;
+
+use Drupal\Component\Serialization\Yaml;
+use Drupal\drupaleasy_repositories\DrupaleasyRepositories\DrupaleasyRepositoriesPluginBase;
+
+/**
+ * Plugin implementation of the drupaleasy_repositories.
+ *
+ * @DrupaleasyRepositories(
+ *   id = "yml_remote",
+ *   label = @Translation("Remote .yml file"),
+ *   description = @Translation("Remote .yml file that includes repository metadata.")
+ * )
+ */
+class YmlRemote extends DrupaleasyRepositoriesPluginBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($uri): bool {
+    $pattern = '|^https?://[a-zA-Z0-9.\-]+/[a-zA-Z0-9_\-.%/]+\.ya?ml$|';
+
+    if (preg_match($pattern, $uri) === 1) {
+      return TRUE;
+    }
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateHelpText(): string {
+    return 'https://anything.anything/anything/anything.yml (or "http")';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRepo(string $uri): array {
+    // file_exists doesn't work with files over http.
+    if (file($uri)) {
+      if ($file_content = file_get_contents($uri)) {
+        $repo_info = Yaml::decode($file_content);
+        $machine_name = array_key_first($repo_info);
+        $repo = reset($repo_info);
+        return $this->mapToCommonFormat($machine_name, $repo['label'], $repo['description'], $repo['num_open_issues'], $uri);
+      }
+    }
+    return [];
+  }
+
+}
